3aa87954970b677e270d4eba001af28f60e82bb6
Cameron Chang <digital.echos@gmail.com>
2021-05-13 23:52:16 +1000

    Add cart styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,6 +6,35 @@ const Cart = () => (
   <div className="cart-container">
     <div className="cart-wrapper">
       <div className="cart-header">Order</div>
+      <div className="cart-content">
+        <ul className="cart-list">
+          <li className="cart-list_item">
+            <div className="cart-list_item-amount">
+              <button className="cart-list_item-amount-minus">-</button>
+              <div className="cart-list_item-amount-multiplier">1</div>
+              <button className="cart-list_item-amount-plus">+</button>
+            </div>
+            <div className="cart-list_item-title">Chicken Burger</div>
+            <div className="cart-list_item-total">$12</div>
+          </li>
+          <li className="cart-list_item">
+            <div className="cart-list_item-amount">
+              <button className="cart-list_item-amount-minus">-</button>
+              <div className="cart-list_item-amount-multiplier">1</div>
+              <button className="cart-list_item-amount-plus">+</button>
+            </div>
+            <div className="cart-list_item-title">Chicken Burger</div>
+            <div className="cart-list_item-total">$12</div>
+          </li>
+        </ul>
+      </div>
+      <div className="cart-footer">
+        <div className="cart-footer-total">
+          <div className="cart-footer-total_title">Total</div>
+          <div className="cart-footer-total_amount">$76.00</div>
+        </div>
+        <button className="cart-footer-button">Pay</button>
+      </div>
     </div>
   </div>
 );
